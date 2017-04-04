1c5e27214da073d29cb4d260e98c3fbb947e50ca
Lieu_Nguyen <rione94@gmail.com>
2017-04-04 09:40:13 +0700

    validate user model, add gem secure pass for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :users
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
