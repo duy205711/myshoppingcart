03b1011dfec3386e9fb9e44f2e878137361c87e1
Lieu_Nguyen <rione94@gmail.com>
2017-04-04 17:49:52 +0700

    edit delete user by admin, fix code style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@
 
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 80 })
+  def gravatar_for(user, options = { size: 60 })
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
