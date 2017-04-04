eeed9797fa3c3e8b3fc3f6d2f04489ba518c734c
Lieu_Nguyen <rione94@gmail.com>
2017-04-04 13:20:02 +0700

    login, logout, show user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 #
 
 class User < ApplicationRecord
-  before_save {self.email.downcase!}
+  before_save { self.email = email.downcase }
   has_many :orders
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -22,4 +22,11 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :users
-  get  '/signup',  to: 'users#new'
+  get 'sessions/new'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
