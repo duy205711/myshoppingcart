2b0da818df39640ba9138a5243ff3f280131afe3
Lieu_Nguyen <rione94@gmail.com>
2017-04-05 14:45:18 +0700

    group validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,19 @@
 #
 
 class User < ApplicationRecord
-  attr_accessor :remember_token
   before_save { self.email = email.downcase }
+
   has_many :orders
+  has_secure_password
+
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  has_secure_password
+  attr_accessor :remember_token
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   # Returns the hash digest of the given string.
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
