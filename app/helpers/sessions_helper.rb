63da993ffe953598d4ce832fa0dc3e07b2c9cff0
Lieu_Nguyen <rione94@gmail.com>
2017-04-05 09:57:13 +0700

    fix code style

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,14 +15,13 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+    return @current_user ||= User.find_by(id: user_id) if (user_id = session[:user_id])
+    user_id = cookies.signed[:user_id]
+    return if user_id.blank?
+    user = User.find_by(id: user_id)
+    if user && user.authenticated?(cookies[:remember_token])
+      log_in user
+      @current_user = user
     end
   end
 
